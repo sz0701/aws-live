bc8aec58b2fa069ae4ada480dec2d61b968ef9b0
sz0701 <96040958+sz0701@users.noreply.github.com>
2024-11-26 01:06:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cbi0icu2uufi.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "employee"
+customregion = "us-east-1"
+
